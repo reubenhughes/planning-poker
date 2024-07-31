d23ecb166e796640ab1bf99637163f7fa09fbe40
Reuben Hughes <reubenhughes@icloud.com>
2024-07-31 09:02:12 +0100

    uninstall dotenv

--- frontend/src/pages/PokerSessionPage.js
+++ frontend/src/pages/PokerSessionPage.js
@@ -1,5 +1,3 @@
-import "dotenv";
-
 import { Realtime } from "ably";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
@@ -15,7 +13,7 @@ import ParticipantList from "../components/ParticipantList";
 import VoteButtonGroup from "../components/VoteButtonGroup";
 
 const ably = new Realtime({
-  key: process.env.ABLY_API_KEY,
+  key: process.env.REACT_APP_ABLY_API_KEY,
   echoMessages: false,
 });
 
